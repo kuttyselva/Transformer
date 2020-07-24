34b30f30ea139d9dfe7b8a66cb9f8bec01b88510
kuttyselva <kuttyselva57@gmail.com>
2020-07-25 01:07:19 +0530

    Initial Commit

---
+++ Transformer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Transformer
+//
+//  Created by kutty selva on 25/07/20.
+//  Copyright © 2020 kutty selva. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Transformer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Transformer
+//
+//  Created by kutty selva on 25/07/20.
+//  Copyright © 2020 kutty selva. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
